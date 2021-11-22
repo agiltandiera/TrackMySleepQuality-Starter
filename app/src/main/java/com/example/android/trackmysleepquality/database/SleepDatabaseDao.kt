dd788f6e00c91f7707306d901bf89dce3827fde7
agiltandiera <agiltandiera27@gmail.com>
2021-11-22 23:10:23 +0800

    Create the DAO

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -17,6 +17,27 @@
 package com.example.android.trackmysleepquality.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Update
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao {
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query
+    fun get(key: Long): SleepNight?
+    ("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+}
